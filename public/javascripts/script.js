0687140bad6738a5685908c11599ade40969fb46
crdavis011 <crdavis011@gmail.com>
2017-12-13 11:20:56 -0700

    lol

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,13 +1,14 @@
-var myApp = angular.module("myApp",["firebase"]);
+var myApp = angular.module("myApp", ["firebase"]);
 myApp.controller("chatController", ["$scope", "$firebaseArray",
- function($scope, $firebaseArray) {
-   var ref = firebase.database().ref().child("messages");
-   $scope.chats = $firebaseArray(ref);
-   $scope.update = function(user) {
-       var newmessage = {from:user.name || "anonymous",body:user.chat};
-       console.log(newmessage);
-       $scope.chats.$add(newmessage);
-       user.chat = "";
-   }
- }
+    function ($scope, $firebaseArray) {
+        var ref = firebase.database().ref().child("users");
+        $scope.chats = $firebaseArray(ref);
+        $scope.update = function (user) {
+            var newmessage = {from: user.name || "anonymous", body: user.chat};
+            console.log(newmessage);
+            $scope.chats.$add(newmessage);
+            user.chat = "";
+        }
+
+    }
 ]);
